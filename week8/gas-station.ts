d58ac8330c853fc4c393bd6c221a7e02570056f3
JAAAAAEMKIM <JAAAAAEMKIM@users.noreply.github.com>
2023-12-14 01:17:52 +0900

    gas-station

---
+++ week8/gas-station.ts
@@ -0,0 +1,27 @@
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+  const n = gas.length;
+
+  let sum = 0;
+  let min = Infinity;
+  let minIdx = -1;
+  for (let i = 0; i < n; i++) {
+    sum = sum + gas[i] - cost[i];
+    if (sum < min) {
+      minIdx = i;
+      min = sum;
+    }
+  }
+
+  if (min > 0) return 0;
+
+  sum = 0;
+  min = Infinity;
+  let cur = minIdx;
+  for (let i = 0; i < n; i++) {
+    cur = (cur + 1) % n;
+    sum = sum + gas[cur] - cost[cur];
+    if (sum < min) min = sum;
+  }
+  if (sum < 0) return -1;
+  return (minIdx + 1) % n;
+}
